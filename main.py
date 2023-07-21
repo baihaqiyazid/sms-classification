f3b694db410bdf7956b0a4e9040944fe569b6d85
baihaqiyazid <baihaqiyazid16@gmail.com>
2023-07-21 22:30:52 +0700

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 import pickle
 
 with open('model/model_sms_classification.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
-
 app = FastAPI()
+templates = Jinja2Templates(directory="templates") 
 
-@app.get("api/check-spam/{text}")
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/api/check-spam/{text}")
 
 def check_spam(text: str):
 
